9b8ec68183b79e0cedd52274af7bf362d8697d40
marcDouglas <mbookmeyer@gmail.com>
2017-11-06 11:25:52 -0600

    moved screens.old to old.screens to avoid tab completion conflict.

--- screens.old/tintTwo/tintTwo.optionButtons.tcl
+++
@@ -1,9 +0,0 @@
-proc optionsButtonCreate { frameOptions } {
-	#button .f.tintTwo.h5 -text "firstButton" -command "readConfig"
-    #grid .f.tintTwo.h5 -column 3 -row 0 
-	
-}
-
-proc initTint2ButtonEditor { } {
-	global buttonKeys2
-}
