41520e37c8ed37ca893fdb56a2b98af8b9a75484
marcDouglas <mbookmeyer@gmail.com>
2017-11-05 20:47:44 -0600

    vader 4

--- screens/syncDrives/bin/syncDrive.flower.sh
+++ screens/syncDrives/bin/syncDrive.flower.sh
@@ -3,7 +3,7 @@
 echo 'guiCMD/screens/syncDrive/bin/syncDrive.flowers.sh is started. '
 echo "."
 
-sudo rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/srv/*","/var/cache/pacman/pkg","home/shawna/.cache/*","/home/shawna/Desktop/movies/*"} / /run/media/shawna/BumbleBee/backups/flower.11.2.17/
+sudo rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/srv/*","/var/cache/pacman/pkg","home/shawna/.cache/*","/home/shawna/Desktop/movies/*","/tmp/*"} / /run/media/shawna/BumbleBee/backups/flower.11.2.17/
 echo "."
 sleep 1
 echo "syncDrive.sh is finished."
--- screens/syncDrives/bin/syncDrive.vader.sh
+++ screens/syncDrives/bin/syncDrive.vader.sh
@@ -5,7 +5,7 @@ echo 'vader is now backing up to wishnu.'
 sleep 1
 echo""
 echo""
-sudo rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/srv/*","home/marc/build/*","/var/cache/pacman/pkg","/home/marc/.android/*","/home/marc/.cache/*","home/marc/.local/share/Steam/*","home/marc/documents/marcDouglas.us/public_html/android-apps/*","/home/marc/downloads/*","/opt/android-ndk/*","/opt/android-studio/*","/home/marc/.kodi/userdata/*","/home/marc/.mozilla/*","/tmp"} / /run/media/marc/wishnu/backups/vader.ssh.bkup
+sudo rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/srv/*","home/marc/build/*","/var/cache/pacman/pkg","/home/marc/.android/*","/home/marc/.cache/*","home/marc/.local/share/Steam/*","home/marc/documents/marcDouglas.us/public_html/android-apps/*","/home/marc/downloads/*","/opt/android-ndk/*","/opt/android-studio/*","/home/marc/.kodi/userdata/*","/home/marc/.mozilla/*","/tmp/*"} / /run/media/marc/wishnu/backups/vader.ssh.bkup
 echo "."
 sleep 1
 echo "syncDrive.vader.sh is finished."
