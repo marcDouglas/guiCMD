affe7fec6a12bc98b6260f43b1b118896ce8366b
marcDouglas <mbookmeyer@gmail.com>
2017-11-07 16:42:07 -0600

    syncHostConnect and syncHostSend seem to be broadcasting and received by mpdSync Servers.

--- screens/mpdController/mpdController.gui.tcl
+++ screens/mpdController/mpdController.gui.tcl
@@ -13,22 +13,27 @@ package require Thread
 
 source avahi/discover.tcl
 
-
+global syncHosts
+array set syncHosts []
+    
+    
 proc mpdController_initialize { } {
+
     labelframe .f.mpdController.localhost -text "localhost" -width 600 -height 100
     #labelframe .f.syncDrives.localhost -text "localhost" -width 600 -height 100
     
-    button .f.mpdController.localhost.h1 -text "Client" -command "start_Echo_Client"
+    button .f.mpdController.localhost.h1 -text "sync" -command "connectClients"
     #set cmd "list album group albumartist"
     button .f.mpdController.localhost.h2 -text "discovery" -command "findServersAvahi"
     button .f.mpdController.localhost.h3 -text "mpdServer:pause" -command "sendTo_mpd_Server2 pause"
-   
-    text .f.mpdController.localhost.t0 -width 80 -height 24
+    button .f.mpdController.localhost.h4 -text "multipause" -command "syncHostSend pause"
+
+    text .f.mpdController.localhost.t0 -width 80 -height 12
     canvas .f.mpdController.localhost.circle -width 25 -height 25 -highlightt 0
     .f.mpdController.localhost.circle create oval 2 2 24 24 -tags t1 -fill green -outline ""
     .f.mpdController.localhost.circle create arc 2 2 24 24 -tags t2 -fill red -extent 359 -outline ""
     #.f.mpdController.localhost.circle itemconfig t2 -extent 180
-    text .f.mpdController.localhost.statusText -width 40 -height 1
+    text .f.mpdController.localhost.statusText -width 20 -height 1
     
     grid .f.mpdController.localhost -column 0 -row 0
     grid .f.mpdController.localhost.h1 -column 0 -row 1
@@ -36,8 +41,9 @@ proc mpdController_initialize { } {
     grid .f.mpdController.localhost.h3 -column 2 -row 1
     grid .f.mpdController.localhost.circle -column 0 -row 0
     grid .f.mpdController.localhost.statusText -column 1 -row 0
+    grid .f.mpdController.localhost.h4 -column 2 -row 0
     # grid .f.mpdController.localhost.m0 -column 3 -row 0
-    grid .f.mpdController.localhost.t0 -column 0 -row 3 -columnspan 3
+    grid .f.mpdController.localhost.t0 -column 0 -row 5 -columnspan 3
     start_Echo_Server
 }
 proc guiTextInsert_mpd {cmdOutput} {
@@ -57,18 +63,80 @@ proc findServersAvahi { } {
     global interfaces
     global ipAddresses
     global ports
+    global hostCheckButtons
     
-    guiTextInsert_mpd $hosts
-    guiTextInsert_mpd $ipAddresses
+   # guiTextInsert_mpd $hosts
+   # guiTextInsert_mpd $ipAddresses
     
     set i 0
+    set row 2
+    set column 0
     foreach host $hosts {
         set tmp "$host [lindex $ipAddresses $i]"
-        checkbutton .f.mpdController.localhost.clients$i -text $tmp
-        grid .f.mpdController.localhost.clients$i -column $i -row 2
+        guiTextInsert_mpd $tmp
+        if { [expr $i / 3] == 0 } {
+            set column $i
+        } else {
+            set column [expr $i % 3 ] 
+        }
+        set row [expr $i / 3 + 2]
+        checkbutton .f.mpdController.localhost.clients$i -text $tmp -variable hostCheckButton($i)
+        grid .f.mpdController.localhost.clients$i -column $column -row $row
         incr i
     }
         
+}
+
+proc connectClients { } {
+    global hosts
+    global interfaces
+    global ipAddresses
+    global ports  
+    global hostCheckButton
+    .f.mpdController.localhost.t0 delete 1.0 end  
+    #clearing log file
+
+    set i -1
+    foreach host $hosts {
+        incr i
+        guiTextInsert_mpd "\[$hostCheckButton($i)\][lindex $ipAddresses $i] [lindex $ports $i]"
+        if { $hostCheckButton($i) ==  1 } {
+              syncHostConnect $i
+        }
+    }
+    
+}
+
+proc syncHostConnect { i } {
+    global hosts
+    global interfaces
+    global ipAddresses
+    global ports
+    global syncHosts
+    guiTextInsert_mpd "connecting:[lindex $hosts $i]:[lindex $ports $i]"
+    set sock [Echo_Client [lindex $hosts $i] [lindex $ports $i]]
+    set syncHosts($i) $sock
+    puts $syncHosts($i) "[info hostname] is connected\n"
+
+    guiTextInsert_mpd "success talking to:[lindex $hosts $i]:[lindex $ports $i]"
+
+    #while {[gets $syncHosts($i) line] >= 0} {
+    #    puts $line
+    #}  
+}
+
+proc syncHostSend { msg } {
+    .f.mpdController.localhost.t0 delete 1.0 end
+    global hosts
+    global interfaces
+    global ipAddresses
+    global ports
+    global syncHosts   
+    foreach {i host} [array get syncHosts] {
+        puts $host "[info hostname] sendTest\n"
+        guiTextInsert_mpd "syncHostSend:[lindex $hosts $i]:[lindex $ports $i]"
+
+    }
 }
 
 
@@ -77,21 +145,6 @@ proc stop_Echo_Server { } {
     guiTextInsert_mpd "Stopping Echo Server."
     set echo_server_state stop
 }
-
-
-
-
-
-
-# Echo_Server --
-#	Open the server listening socket
-#	and enter the Tcl event loop
-#
-# Arguments:
-#	port	The server's port number
-
-
-
 
 proc Echo_Server {port} {
     global echo_server_state
@@ -222,20 +275,13 @@ proc sendTo_mpd_Server { } {
 }
 
 proc sendTo_mpd_Server2 { cmd } {
-   # global m
     set m [Echo_Client localhost 6600]
-   # start_mpd_Client_Only_Once
-    
-    #puts $m "pause\n"
+
     puts $m "$cmd\n"
-   # puts $m "Joyful!"
-   # puts $m "Fellow!"
+
     while {[gets $m line] >= 0} {
         puts $line
     }
-    #gets $m line
-    #puts $line
-    #gets $m line
-    #puts $line
+
 
 }
